6f8fb6982ebae0f695994cb2e3f1598b6b29f9c1
Konopvik <konopatenkoviktor@gmail.com>
2023-08-25 15:18:49 +0200

    ADD data to index.js, add props

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 
 
-function App() {
+function App(props) {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
@@ -18,8 +18,8 @@ function App() {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Routes>
-                        <Route path="/profile" element={<Profile/>}/>
-                        <Route path="/dialogs/*" element={<Dialogs/>}/>
+                        <Route path="/profile" element={<Profile posts={props.posts}/>}/>
+                        <Route path="/dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
                         <Route path="/news" element={<News/>}/>
                         <Route path="/music" element={<Music/>}/>
                         <Route path="/settings" element={<Settings/>}/>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,26 +4,12 @@ import DialogItem from "./DialogItem/DialogsItem";
 import Message from "./Message/Message";
 
 
-const Dialogs = () => {
+const Dialogs = (props) => {
 
-    let dialogs = [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrey'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Viktor'},
-        {id: 6, name: 'Valera'},
-    ]
 
-    let messages = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How is your it-kamasutra?'},
-        {id: 3, message: 'Yo'},
-    ]
+    let dialogsElements = props.dialogs.map(d => (<DialogItem name={d.name} id={d.id}/>))
 
-    let dialogsElements = dialogs.map(d => (<DialogItem name={d.name} id={d.id}/>))
-
-    let messagesElements = messages.map(m => (<Message message={m.message}/>))
+    let messagesElements = props.messages.map(m => (<Message message={m.message}/>))
 
     return (
         <div className={s.dialogs}>
