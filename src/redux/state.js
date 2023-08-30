26f646ee74136540f1e2f7c402de3d69a3ec1d8f
Konopvik <konopatenkoviktor@gmail.com>
2023-08-30 17:47:18 +0200

    Add file render.js, add creating posts

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,4 @@
+import {rerenderEntireTree} from "../render";
 
 
 let state = {
@@ -31,6 +32,7 @@ export let addPost = (postMessage) => {
         likesCount: 0
     }
     state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
 }
 
 
