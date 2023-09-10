09d59f9229390b3861bd9ebdc7acb289a6d06a0b
Konopvik <konopatenkoviktor@gmail.com>
2023-09-10 20:48:07 +0200

    action creator, action type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
-import {updateNewPostText} from "../../../redux/state";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
+
 
 const MyPosts = (props) => {
-
 
     let postsElements = props.posts.map(p => (<Post message={p.message} likesCount={p.likesCount}/>))
 
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.dispatch({ type: 'ADD-POST'})
+        props.dispatch(addPostActionCreator())
         newPostElement.current.value = ''
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text};
-        props.dispatch(action)
+        props.dispatch(updateNewPostTextActionCreator(text))
     }
 
     return (
