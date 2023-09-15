4aa8d19171fdc3ec4e2b05adde9003a941d0e768
Konopvik <konopatenkoviktor@gmail.com>
2023-09-15 14:04:42 +0200

    connect, mapStateToProps, mapDispatchToProps

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import store from './redux/redux-store'
 import reportWebVitals from "./reportWebVitals";
-import StoreContext, {Provider} from "./StoreContext";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
