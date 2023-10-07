ea440026714d1da0c62e8a31aa75da457d1327ad
Konopvik <konopatenkoviktor@gmail.com>
2023-10-07 12:50:59 +0200

    Cookie, login, auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img src='https://img.freepik.com/free-icon/logo_318-195518.jpg'/>
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login :
+            <NavLink to={'/login'}>Login</NavLink> }
+        </div>
         </header>
     );
 }
