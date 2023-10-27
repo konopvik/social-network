ddf10febadb1196addf5b581796d3515fbf9e951
Konopvik <konopatenkoviktor@gmail.com>
2023-10-27 11:43:05 +0200

    stopSubmit (redux-form)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,7 +5,7 @@ import {required} from "../../utils/validators/validators";
 import {connect} from "react-redux";
 import {login} from "../../redux/auth-reducer";
 import {Navigate} from "react-router-dom";
-import {mapStateToPropsFactory} from "react-redux/es/connect/mapStateToProps";
+import styles from "../common/FormsControls/FormsControls.module.css"
 
 const LoginForm = (props) => {
     return (
@@ -25,6 +25,9 @@ const LoginForm = (props) => {
                 <Field component={Input} name={"rememberMe"}
                        type={"checkbox"}/> remember me
             </div>
+            { props.error && <div className={styles.formSummaryError}>
+                {props.error}
+            </div> }
             <div>
                 <button>Login</button>
             </div>
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -19,7 +19,7 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
-            userId = 30052;
+            userId = this.props.authorizedUserId;
         }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
@@ -35,6 +35,8 @@ class ProfileContainer extends React.Component {
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
     status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth
 
 })
 export default compose(
